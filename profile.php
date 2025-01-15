790b9e22e53f694e1e5ced042b4a32dd7c6bb001
Jorge Cortés <jorgecortes@Mac.station>
2025-01-15 11:30:42 +0100

    [actualizo conexion bbdd user y pass]

--- profile.php
+++ profile.php
@@ -303,8 +303,8 @@ function recuperarUserDataDePrueba(){
     try {
         $hostname = "localhost";
         $dbname = "DatingApp";
-        $username = "admin";
-        $pw = "admin123";
+        $username = "root";
+        $pw = "1234";
         $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", "$username", "$pw");
     } catch (PDOException $e) {
         echo "Failed to get DB handle: " . $e->getMessage() . "\n";
