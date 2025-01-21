ff23e6575504a89073a0d0c1145710288ec19cf6
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-01-21 03:13:11 +0100

    [ 19 - FIX ] - fix carrousel

--- discover.js
+++ discover.js
@@ -116,78 +116,77 @@ async function downloadData(index) {
             if (misDatos.length > 0) {
                 misDatos.forEach(usuario => {
                    
-                        if(usuario["TotalPoints"] != 0){
-                            // contenedor genérico de la carta
-                            const newCard = $("<div>").attr("class", "card");
-                            newCard.attr("data-user-id", usuario["IdUser"]);  // Asignamos el id de usuario
+                    if(usuario["TotalPoints"] != 0){
 
-                            // creación del contenedor del carrusel
-                            const carouselContainer = $("<div>").attr("class", "carousel-container card-img");
+                        // contenedor genérico de la carta
+                        const newCard = $("<div>").attr("class", "card");
+                        newCard.attr("data-user-id", usuario["IdUser"]);  // Asignamos el id de usuario
+                        
+                        // creación del contenedor del carrusel -> img + puntitos
+                        const carouselContainer = $("<div>").attr("class", "carousel-container card-img");
+                        // creación del contenedor de las fotos
+                        const photosContainer = $("<div>").attr("class", "photos-container");
 
+                        // variables de control de imágenes para la creación de los puntitos
+                        const images = [];
+                        let imageIndex = 0;
+
+                        // creación imágenes del carrusel
+                        for (let i = 0; usuario[`img${i}`]; i++) {
+                            const img = $("<img>")
+                                .attr("src", usuario[`img${i}`])
+                                .attr("class", "carousel-img")
+                                .css("display", i === 0 ? "block" : "none");
                             
-                            // creación del contenedor de las fotos
-                            const photosContainer = $("<div>").attr("class", "photos-container");
-                            
-                            // variables de control de imágenes para la creación de los puntitos
-                            const images = [];
-                            let imageIndex = 0;
-
-                            // creación imágenes del carrusel
-                            for (let i = 0; usuario[`img${i}`]; i++) {
-                                const img = $("<img>")
-                                    .attr("src", usuario[`img${i}`])
-                                    .attr("class", "carousel-img")
-                                    .css("display", i === 0 ? "block" : "none");
-                                
-                                // añadido de la foto en el contenedor de fotos
-                                photosContainer.append(img);
-                                images.push(img);
-                            }
-
-
-                            // Crear puntitos carrusel
-                            const dotsContainer = $("<div>").attr("class", "dots-container");
-                            for (let i = 0; i < images.length; i++) {
-                                const dot = $("<span>")
-                                    .attr("class", `dot ${i === 0 ? "active" : ""}`)
-                                    .on("click", () => {
-                                        // Evento al hacer click en el puntito
-                                        images[imageIndex].css("display", "none");
-                                        $(".dot").eq(imageIndex).removeClass("active");
-                                        imageIndex = i;
-                                        images[imageIndex].css("display", "block");
-                                        $(".dot").eq(imageIndex).addClass("active");
-                                    });
-                                dotsContainer.append(dot);
-                            }
-
-                            // Añadir evento de clic en la imagen para pasar a la siguiente
-                            carouselContainer.on("click", () => {
-                                images[imageIndex].css("display", "none");
-    
-                                // Limitar la selección de dots al contenedor actual
-                                const dots = carouselContainer.find(".dot");
-                                dots.eq(imageIndex).removeClass("active");
-                            
-                                imageIndex = (imageIndex + 1) % images.length; // Siguiente imagen (vuelve al inicio si es la última)
-                                
-                                images[imageIndex].css("display", "block");
-                                dots.eq(imageIndex).addClass("active");
-                            });
-
-
-                            // añadir el contendor de fotos y de puntitos en el contenedor del carousel y añadir el carrousel al contenedor general
-                            carouselContainer.append(photosContainer,dotsContainer);
-                            newCard.append(carouselContainer);
-                            
-                            // añadir la información al contenedor general
-                            const inform = $("<div>").attr("class", "card-info");
-                            inform.append($("<h2>").text(usuario["Username"] + ", " + usuario["UserAge"]));
-                            newCard.append(inform);
-                            
-                            contenedor.prepend(newCard);  // Insertamos la nueva tarjeta al principio
-                            contenedor.css("background", "white");  // Cambiamos el estilo del contenedor
+                            // añadido de la foto en el contenedor de fotos
+                            photosContainer.append(img);
+                            images.push(img);
                         }
+
+                        // Crear puntitos carrusel
+                        const dotsContainer = $("<div>").attr("class", "dots-container");
+                        for (let i = 0; i < images.length; i++) {
+                            const dot = $("<span>")
+                                .attr("class", `dot ${i === 0 ? "active" : ""}`)
+                                .on("click", () => {
+                                    // Evento al hacer click en el puntito
+                                    images[imageIndex].css("display", "none");
+                                    $(".dot").eq(imageIndex).removeClass("active");
+                                    imageIndex = i;
+                                    images[imageIndex].css("display", "block");
+                                    $(".dot").eq(imageIndex).addClass("active");
+                                });
+                            dotsContainer.append(dot);
+                        }
+
+                        // Añadir evento de clic en la imagen para pasar a la siguiente
+                        carouselContainer.on("click", () => {
+                            images[imageIndex].css("display", "none");
+
+                            // Limitar la selección de dots al contenedor actual
+                            const dots = carouselContainer.find(".dot");
+                            dots.eq(imageIndex).removeClass("active");
+                        
+                            imageIndex = (imageIndex + 1) % images.length; // Siguiente imagen (vuelve al inicio si es la última)
+                            
+                            images[imageIndex].css("display", "block");
+                            dots.eq(imageIndex).addClass("active");
+                        });
+                        
+                        // añadir el contendor de fotos y de puntitos en el contenedor del carousel y añadir el carrousel al contenedor general
+                        carouselContainer.append(photosContainer,dotsContainer);
+                        newCard.append(carouselContainer);
+
+                        // añadir la información al contenedor general
+                        const inform = $("<div>").attr("class", "card-info");
+                        inform.append($("<h2>").text(usuario["Username"] + ", " + usuario["UserAge"]));
+                        newCard.append(inform);
+
+                        contenedor.prepend(newCard);  // Insertamos la nueva tarjeta al principio
+                            contenedor.css("background", "white");  // Cambiamos el estilo del contenedor
+
+                    }
+
                 });
             } else{
                 // Si no hay más perfiles, mostramos un mensaje
