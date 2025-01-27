7ed3e4820e67610764f4d23cf5ee5245e6ace804
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-01-27 18:30:43 +0100

    [FIX - HEADER]

--- admin/header.php
+++ admin/header.php
@@ -1,18 +1,21 @@
 <header>
 
     <?php
-        $rutaActual = basename($_SERVER['REQUEST_URI']);
-
-        if($rutaActual === 'index.php'){
+        $rutaActual = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        $queryString = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+        
+        if ($rutaActual === 'index.php') {
             echo '<a class="btn" onclick="deleteSession()">Salir</a>';
         }
         else if ($rutaActual === 'users.php' || $rutaActual === 'logs.php') {
-            echo '<a href="index.php"><img src="/images/flecha-izquierda.png" alt="flecha para volver al panel de administrador"></a>';
-        }
-        else{
-           // Obtener la página anterior
-           $paginaAnterior = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
-           echo "<a href='{$paginaAnterior}'><img src='/images/flecha-izquierda.png' alt='volver'></a>";
+            
+            if (isset($queryString) && strpos($queryString, 'page=') !== false) {
+                echo '<a href="index.php"><img src="/images/flecha-izquierda.png" alt="flecha para volver al panel de administrador"></a>';
+            } else {
+               
+                $paginaAnterior = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
+                echo "<a href='{$paginaAnterior}'><img src='/images/flecha-izquierda.png' alt='volver'></a>";
+            }
         }
         
     ?>
